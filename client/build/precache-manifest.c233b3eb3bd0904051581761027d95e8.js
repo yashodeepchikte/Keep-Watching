e23d35fd17e763414252a5f1c30150c83438fdb4
yashodeepchikte <yashodeepchikte@gmail.com>
2020-06-26 05:39:36 +0530

    Gitignore updated

---
+++ client/build/precache-manifest.c233b3eb3bd0904051581761027d95e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f783292edf11ff93b7c847ea7463cba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19529baf2a4b5dc87cb",
+    "url": "/static/js/2.876e9831.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.876e9831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f2d4f2ea5845d70097",
+    "url": "/static/js/main.9d378da5.chunk.js"
+  },
+  {
+    "revision": "6b71224ab26933836c3b",
+    "url": "/static/js/runtime-main.9009ef15.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
